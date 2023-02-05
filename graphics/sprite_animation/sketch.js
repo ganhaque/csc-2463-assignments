9ce941c28697313030175593ad9e694fdcfb8a12
ganhaque <ganahquegnq@gmail.com>
2023-02-04 23:22:07 -0600

    :broom: :sparkles: clean out comment and add non-grid movement

--- graphics/sprite_animation/sketch.js
+++ graphics/sprite_animation/sketch.js
@@ -43,13 +43,6 @@ var colorPallete = [
 var isGridMovement = true;
 var showGrid = true;
 
-// var keyDelay = 2;
-// var increment = 0;
-
-//516 44
-//56 40
-//9 per
-
 ///////////////////////////////////////////////////////////////
 function preload() {
   spriteSheet = loadImage("assets/gameplay_sprites.png");
@@ -58,9 +51,6 @@ function preload() {
 function setup() {
   frameRate(24);
   createCanvas(tileLength*canvasWidthScaling, tileLength*canvasHeightScaling);
-
-  // spriteSheet.loadPixels();
-  // var pixels = spriteSheet.pixels;
 
   characterArray.push(
     new Character(
@@ -73,52 +63,17 @@ function setup() {
 }
 
 ///////////////////////////////////////////////////////////////
-
 function draw() {
   background(canvasColor);
   if(showGrid) {
     drawGrid(canvasWidthScaling, canvasHeightScaling, 'rgb(20, 20, 255, 0.75)');
   }
-
-  // if(keyIsDown(RIGHT_ARROW) && frameCount % 6 === 0 & increment < keyDelay) {
-  //   increment += 1;
-  //   console.log("HODL:", increment);
-  // }
-  //
-  // if(keyIsDown(RIGHT_ARROW) && frameCount % 4 === 0 & increment >= keyDelay) {
-  //   // increment += 1;
-  //   // console.log("HODL:", increment);
-  //   console.log("proc");
-  //   for(var i = 0; i < characterArray.length; i++) {
-  //     characterArray[i].rightMovement();
-  //   }
-  // }
-
   for(var i = 0; i < characterArray.length; i++) {
     characterArray[i].draw();
   }
-  // console.log("framerate:", frameRate());
-  // image(spriteSheet, 0, 0, 24, 24, 24, 0, 24, 24)
-  // image(
-  //   spriteSheet, 0, 0,
-  //   tileLength, tileLength,
-  //   0,
-  // 0,
-  //   24, 24
-  // )
-  // for(let i = 0; i < palleteArray.length; i++) {
-  //   palleteArray[i].draw();
-  // }
-  // if (renderBrushSizeIsTRUE) {
-  //   renderBrushSize();
-  // }
-  // if (displayStatusBar) {
-  //   renderStatusBar();
-  // }
 }
 
 ///////////////////////////////////////////////////////////////
-
 function keyPressed() {
   for(var i = 0; i < characterArray.length; i++) {
     characterArray[i].normalMovementTap(LEFT_ARROW, DOWN_ARROW, UP_ARROW, RIGHT_ARROW);
@@ -139,7 +94,6 @@ class Character {
     spriteXCoordinate, spriteYCoordinate,
     walkingAnimationLength, numberofVariation,
     // color = "#FFFFFF",
-    // border = 0,
     sizeScaling = 1,
     hasLeftRight = true,
     hasDownUp = true,
@@ -155,7 +109,6 @@ class Character {
     this.numberofVariation = numberofVariation;
 
     // this.color = color;
-    // this.border = border;
     this.sizeScaling = sizeScaling;
 
     this.velocity = 0;
@@ -175,17 +128,23 @@ class Character {
     this.hasLeftRight = hasLeftRight;
     this.hasDownUp = hasDownUp;
 
-    this.movementSpeed = 1;
+    if(isGridMovement) {
+      this.movementSpeed = 24;
+    }
+    else {
+      this.movementSpeed = 8;
+    }
     this.wobbleSpeed = 1;
 
     this.holdKeyDelay = 1;
     this.holdKeyCounter = 0;
 
-    this.sleepIsForTheWeak = true;
+    this.sleepIsForTheWeak = false;
     this.idleCounter = 0;
     this.idleDelay = 2;
   }
 
+///////////////////////////////////////////////////////////////
   draw() {
     if(frameCount % (10*this.wobbleSpeed) === 0) {
       this.wobble();
@@ -193,11 +152,9 @@ class Character {
     let movementKeyIsDown = (keyIsDown(RIGHT_ARROW) || keyIsDown(LEFT_ARROW) || keyIsDown(UP_ARROW) || keyIsDown(DOWN_ARROW));
     if(frameCount % 1 === 0 && movementKeyIsDown) {
       this.holdKeyCounter += 0.2;
-      console.log("HODL:", this.holdKeyCounter);
+      // console.log("HODL:", this.holdKeyCounter);
     }
     if(frameCount % 3 === 0 && this.holdKeyCounter > this.holdKeyDelay) {
-      // this.rightMovement();
-      // this.normalMovementTap();
       if(keyIsDown(RIGHT_ARROW)) {
         console.log("proc");
         this.rightMovement();
@@ -220,7 +177,7 @@ class Character {
       if(frameCount % 24 === 0) {
         if(this.idleCounter <= this.idleDelay) {
           this.idleCounter += 1;
-          console.log("inceased counter:", this.idleCounter)
+          console.log("sleep counter:", this.idleCounter)
         }
       }
       if(this.idleCounter > this.idleDelay) {
@@ -228,26 +185,8 @@ class Character {
       }
     }
 
-    // for( this.acceleration < 2; this.acceleration++) {
     push();
-    // scale(this.xDirection, this.yDirection);
-    // if (this.xDirection === 0 && this.yDirection === 0) {
-    //   console.log("yDirection is", this.yDirection)
-    //   scale(1, this.yDirection);
-    // }
-    // else if (this.yDirection === 0 && this.xDirection === 0){
-    //   console.log("xDirection is", this.xDirection)
-    //   scale(this.xDirection, 1);
-    // }
-    // this.locationX += this.velocity * this.xDirection;
-    // console.log("locationX is:", this.locationX);
-    // this.locationY += this.velocity * this.yDirection;
-    // console.log("update");
-    // console.log("locationX, locationY is", this.locationX, this.locationY)
-    // translate(this.locationX, this.locationY);
     noSmooth();
-    // translate(this.velocity * this.xDirection, this.velocity * this.yDirection); //uncomment for glitchy fx
-    // fill(this.color);
     image(
       spriteSheet, this.initalX + this.locationX, this.initalY + this.locationY,
       tileLength*this.sizeScaling, tileLength*this.sizeScaling,
@@ -257,19 +196,13 @@ class Character {
     )
     pop();
 
-
     if(isGridMovement) {
       this.velocity = 0;
       this.movementSpeed = tileLength;
     }
-
-    // this.normalMovementHold();
-    // while(this.acceleration < 1) {
-    //   this.rightMovement();
-    //   this.acceleration += 1;
-    // }
   }
 
+///////////////////////////////////////////////////////////////
   wobble() {
     if(this.currentVariation >= this.amountOfVariation - 1) {
       this.currentVariation = 0;
@@ -279,6 +212,7 @@ class Character {
     }
   }
 
+///////////////////////////////////////////////////////////////
   idleBabaIsSleep() {
     if(this.xDirection === 1) {
       this.currentFrame = 0;
@@ -294,6 +228,22 @@ class Character {
     }
   }
 
+  spriteIsReset() {
+    if(this.xDirection === 1) {
+      this.currentFrame = 1;
+    }
+    else if(this.xDirection === -1) {
+      this.currentFrame = 11;
+    }
+    else if(this.yDirection === -1) {
+      this.currentFrame = 6;
+    }
+    else if(this.yDirection === 1) {
+      this.currentFrame = 16;
+    }
+  }
+
+///////////////////////////////////////////////////////////////
   leftMovement() {
     console.log("move left")
     if(this.yDirection === 0 && this.xDirection === -1) {
@@ -370,16 +320,18 @@ class Character {
     this.updateLocation();
   }
 
+///////////////////////////////////////////////////////////////
   updateLocation() {
     this.locationX += this.velocity * this.xDirection;
     this.locationY += this.velocity * this.yDirection;
     if(isGridMovement) {
       this.velocity = 0;
-      this.movementSpeed = 24;
+      // this.movementSpeed = 24;
     }
     this.idleCounter = 0;
   }
 
+///////////////////////////////////////////////////////////////
   normalMovementTap(moveLeftKey = LEFT_ARROW, moveDownKey = DOWN_ARROW, moveUpKey = UP_ARROW, moveRightKey = RIGHT_ARROW) {
     switch(keyCode) {
       case moveLeftKey:
@@ -404,12 +356,14 @@ class Character {
       this.velocity = 0;
       this.idleCounter = 0;
       this.holdKeyCounter = 0;
+      if(!isGridMovement){
+        this.spriteIsReset();
+      }
     }
   }
 }
 
 ///////////////////////////////////////////////////////////////
-
 function drawGrid(column, row, color) {
   for (var x = 0; x < width; x += width / column) {
     for (var y = 0; y < height; y += height / row) {
@@ -427,108 +381,3 @@ function drawGrid(column, row, color) {
 }
 
 ///////////////////////////////////////////////////////////////
-// function keyTyped() {
-//   switch(key) {
-//     case 'c':
-//       //toggle eraser
-//       if(isEraser) {
-//         console.log("brush mode")
-//         isEraser = false;
-//       }
-//       else {
-//         console.log("eraser mode")
-//         isEraser = true;
-//       }
-//       break;
-//     // case 's':
-//     //   if(renderBrushSizeIsTRUE) {
-//     //     console.log("renderBrushSizeIsTRUE mode")
-//     //     renderBrushSizeIsTRUE = false;
-//     //   }
-//     //   else {
-//     //     console.log("renderBrushSizeIsFALSE mode")
-//     //     renderBrushSizeIsTRUE = true;
-//     //   }
-//     //   break;
-//     case 'q':
-//       console.log("q is pressed")
-//       //decrease brush size
-//       if(brushStrokeIndex >= 1) {
-//         brushStrokeIndex -= 1;
-//         console.log("brush size:", brushStrokeArray[brushStrokeIndex]);
-//       }
-//       else {
-//         console.log("cannot decrease brush stroke further")
-//       }
-//       break;
-//     case 'w':
-//       console.log("w is pressed")
-//       //decrease brush size
-//       if(brushStrokeIndex < brushStrokeArray.length -1) {
-//         brushStrokeIndex += 1;
-//         console.log("brush size:", brushStrokeArray[brushStrokeIndex]);
-//       }
-//       else {
-//         console.log("cannot increase brush stroke further")
-//       }
-//       break;
-//     case ' ':
-//       console.log("<leader> is pressed")
-//       //statusbar toggle
-//       if(displayStatusBar) {
-//         console.log("disabled statusbar")
-//         displayStatusBar = false;
-//         deleteStatusBar();
-//       }
-//       else {
-//         console.log("statusbar enabled")
-//         displayStatusBar = true;
-//       }
-//       break;
-//     default:
-//     console.log("not a shortcut");
-//   }
-// }
-
-///////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////
-// function rgbToHex(r, g, b) {
-//   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-// }
-//
-// function extractTheme(sprite, xCoordinateOfTopCorner, yCordinateOfTopCorner, palleteWidth, palleteHeight, colorSquareSize) {
-//   //initialize an appropriate 2D array
-//   var themeArray = new Array(height);
-//   for(var i = 0; i < height; i++) {
-//     themeArray[i] = new Array(width);
-//   }
-//
-//   //load color into the array
-//   let startingPixel = "what";
-//   let endingPixel = "what";
-//   let wholeThing = 4 * (palleteWidth * pixelDensity() * (height * pixelDensity()));
-//   for(var i = 4*palleteWidth; i < wholeThing; i += 4) {
-//     for (var j = 4*palleteHeight; i < wholeThing; i += 4) {
-//       theme[j/4/palleteHeight - 1][i/4/palleteWidth] = rbgToHex(pixels[1])
-//
-//     }
-//   }
-// }
-// function initialize_color_pallete(item, index, arr) {
-//   // console.log("index:",index);
-//   // console.log("color:",item);
-//   // console.log("array length:", palleteArray.length);
-//   palleteArray.push(
-//     new PalleteSquare(initialX, initialY + index * initialSize, initialSize, item)
-//   );
-// }
-
-// class Theme {
-//   constructor(colorSheet, xCoordinateOfTopCorner, yCordinateOfTopCorner, palleteWidth, palleteHeight, colorSquareSize) {
-//     this.colorSheet = colorSheet;
-//
-//   }
-//
-// }
-
