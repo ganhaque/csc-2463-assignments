0eef72007f25c1f05c797f9fcacd339a224b0fc1
ganhaque <ganhaquegnq@gmail.com>
2023-02-13 13:17:45 -0600

    :construction: gamestate, score, and font WIP

--- graphics/bug_squish/sketch.js
+++ graphics/bug_squish/sketch.js
@@ -257,22 +257,55 @@ let moveSoundCounter = 0;
 let moveSoundArray = [];
 
 let bugLevel = 0;
+const GAME_STATE = {
+  StartingScreen: "StartingScreen",
+  Playing: "Playing",
+  GameOver: "GameOver"
+};
+
+let scoreData = { score: 0, highScore: 0 };
+// let gameData = { timePerLevel: 10, elapsedTime: 0, amountOfBug: 16, state: GAME_STATE.StartingScreen };
+let gameData = { timePerLevel: 10, elapsedTime: 0, amountOfBug: 16, state: GAME_STATE.StartingScreen };
 
 ///////////////////////////////////////////////////////////////
 function preload() {
   // soundFormats('ogg', 'mp3');
   // moveSoundArray.push(loadSound('assets/sound_fx/047'));
   spriteSheet = loadImage("assets/bug2.png");
+  myFont = loadFont('assets/CozetteVector.ttf');
 }
 
 function setup() {
-  frameRate(60);
+  // frameRate(60);
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  restart();
   // createCanvas(displayWidth, displayHeight);
   // bugArray.push(new Bug(spriteSheet, 8*TILE_LENGTH, 5*TILE_LENGTH, TILE_LENGTH, 1, "h", 1, 1, 1 ));
   // bugArray.push(new Bug(spriteSheet, 8000, 5000, 2400, 1, "v", 1, 0.5, 1 ));
-  bugArray.push(new Bug(spriteSheet, 200, 400, 600, 960, "v", 1, -10, 0.5 ));
-  bugArray.push(new Bug(spriteSheet, 200, 400, 600, 960, "h", 1, -10, 0.5 ));
+
+  // bugArray.push(new Bug(spriteSheet, 200, 400, 600, 960, "v", 1, -10, 0.5 ));
+  // bugArray.push(new Bug(spriteSheet, 200, 400, 600, 960, "h", 1, -10, 0.5 ));
+  //   for(let i=0; i < random(12,20); i++) {
+  //     let movementVariation;
+  //     if(round(random([0,1]))) {
+  //       movementVariation = "v";
+  //     }
+  //     else {
+  //       movementVariation = "h";
+  //     }
+  //     bugArray.push(new Bug(
+  //       spriteSheet,
+  //       random(200, 1600), //starting X
+  //       random(200, 800), //starting Y
+  //       600,
+  //       960,
+  //       movementVariation,
+  //       1,
+  //       -1*random(7,15), //movement speed
+  //       random(0.5,1) //size scaling
+  //     ));
+  //   }
 }
 
 // spriteSheet,
@@ -285,17 +318,98 @@ function setup() {
 
 ///////////////////////////////////////////////////////////////
 function draw() {
-  background(CANVAS_COLOR);
   // renderPlayground(CANVAS_WIDTH, CANVAS_HEIGHT, CANVAS_COLOR);
-  for(let i = 0; i < bugArray.length; i++) {
-    bugArray[i].draw();
+  background(CANVAS_COLOR);
+  switch(gameData.state) {
+    case GAME_STATE.StartingScreen:
+      scoreData.highScore = max(scoreData.score, scoreData.highScore);
+      fill(255);
+      textFont(myFont);
+      textSize(176);
+      textAlign(CENTER);
+      text("Bug Squish!",width/2,height/2 - 76);
+      // textSize(76);
+      // text("Score: " + scoreData.score,width/2,height/2 + 60);
+      // text("Max Score: " + scoreData.highScore,width/2,height/2 + 136);
+      textSize(76);
+      text("Press s to start", width/2, height/2 + 136);
+      break;
+    case GAME_STATE.Playing:
+      for(let i = 0; i < bugArray.length; i++) {
+        bugArray[i].draw();
+      }
+      renderScoreBoard();
+      break;
+    case GAME_STATE.GameOver:
+      scoreData.highScore = max(scoreData.score, scoreData.highScore);
+      fill(255);
+      textFont(myFont);
+      textSize(176);
+      textAlign(CENTER);
+      text("Game Over!",width/2,height/2 - 76);
+      textSize(76);
+      text("Score: " + scoreData.score,width/2,height/2 + 60);
+      text("Max Score: " + scoreData.highScore,width/2,height/2 + 136);
+      textSize(76);
+      text("Press r to restart", width/2, height/2 + 360);
+      break;
   }
   //sprite searcher //DO NOT CLEAN UP
   // noSmooth();
   // image(spriteSheet, 0, 200, 220, 400, 600, 0, 600, 960);
   // image(spriteSheet, 0, 200, 220, 400, 0, 1016, 600, 960);
-  image(spriteSheet, 0, 200, 400, 220, 0, 1945, 960, 600);
+  // image(spriteSheet, 0, 200, 400, 220, 0, 1945, 960, 600);
   // image(spriteSheet, 0, 200, 165, 250, 2500, 0, 500, 825);
+}
+
+///////////////////////////////////////////////////////////////
+function restart() {
+  gameData.elapsedTime = 0;
+  gameData.score = 0;
+  gameData.amountOfBug = random(12, 30);
+
+  bugArray = [];
+  for(let i = 0; i < gameData.amountOfBug; i++) {
+    let movementVariation;
+    if(round(random([0,1]))) {
+      movementVariation = "v";
+    }
+    else {
+      movementVariation = "h";
+    }
+    bugArray.push(new Bug(
+      spriteSheet,
+      random(200, 1600), //starting X
+      random(200, 800), //starting Y
+      600,
+      960,
+      movementVariation,
+      1,
+      -1*random(6,24), //movement speed
+      random(0.25,1) //size scaling
+    ));
+  }
+}
+
+function renderScoreBoard() {
+  push();
+  fill(255);
+  textFont(myFont);
+  textAlign(RIGHT);
+  textSize(86);
+  text(scoreData.score,60,82);
+  let currentTime = gameData.timePerLevel - gameData.elapsedTime;
+  text(ceil(currentTime), width - 40,82);
+  // text(ceil(deltaTime), width - 60,40);
+  gameData.elapsedTime += deltaTime / 1000;
+  // console.log("elapsed time:", gameData.elapsedTime);
+
+  // gameData.elapsedTime += deltaTime;
+  pop();
+
+  if(currentTime < 0) {
+    gameData.state = GAME_STATE.GameOver;
+  }
 }
 
 ///////////////////////////////////////////////////////////////
@@ -347,7 +461,7 @@ class Bug {
           this.onlyMoveHorizontally = true;
           this.spriteWidth = spriteHeight;
           this.spriteHeight = spriteWidth;
-          this.destinationX = 400;
+          this.destinationX = 340;
           this.destinationY = 220;
           this.spriteXCoordinate = 0;
           this.spriteYCoordinate = 1945;
@@ -357,7 +471,7 @@ class Bug {
           this.spriteWidth = spriteWidth;
           this.spriteHeight = spriteHeight;
           this.destinationX = 220;
-          this.destinationY = 400;
+          this.destinationY = 340;
           this.spriteXCoordinate = 0;
           this.spriteYCoordinate = 0;
           break;
@@ -392,8 +506,10 @@ class Bug {
     this.checkCanvasCollision();
 
 
+    let baseFramerate = 1*this.movementSpeed;
+    let proportionalFramerate = round(frameRate() / baseFramerate);
     if(this.isAlive) {
-      if(frameCount % 4 === 0) {
+      if(frameCount % proportionalFramerate === 0) {
         // console.log("call");
         this.animationHandler();
       }
@@ -406,23 +522,25 @@ class Bug {
     // console.log(this.hitboxCheck(mouseX, mouseY));
     // this.hitboxCheck(mouseX, mouseY);
     // image(spriteSheet, 0, 200, 220, 400, 0, 0, 600, 960);
-    if(this.onlyMoveVertically) {
-      image(
-        spriteSheet, this.initalX, this.initalY,
-        this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
-        this.spriteXCoordinate + (this.spriteWidth * this.currentFrame),
-        this.spriteYCoordinate,
-        this.spriteWidth, this.spriteHeight
-      )
-    }
-    else if(this.onlyMoveHorizontally) {
-      image(
-        spriteSheet, this.initalX, this.initalY,
-        this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
-        this.spriteXCoordinate,
-        this.spriteYCoordinate + (this.spriteHeight * this.currentFrame),
-        this.spriteWidth, this.spriteHeight
-      )
+    if(this.isAlive) {
+      if(this.onlyMoveVertically) {
+        image(
+          spriteSheet, this.initalX, this.initalY,
+          this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
+          this.spriteXCoordinate + (this.spriteWidth * this.currentFrame),
+          this.spriteYCoordinate,
+          this.spriteWidth, this.spriteHeight
+        )
+      }
+      else if(this.onlyMoveHorizontally) {
+        image(
+          spriteSheet, this.initalX, this.initalY,
+          this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
+          this.spriteXCoordinate,
+          this.spriteYCoordinate + (this.spriteHeight * this.currentFrame),
+          this.spriteWidth, this.spriteHeight
+        )
+      }
     }
     pop();
   }
@@ -462,7 +580,7 @@ class Bug {
     //
     // }
     if(this.onlyMoveHorizontally === true) {
-      this.velocity = calculateSide(this.movementSpeed, 0)
+      this.velocity = calculateSide(this.movementSpeed, 0) //return movementspeed * 1
       this.initalX += this.velocity;
       // translate(this.initalX, this.initalY);
       // rotate(PI/2);
@@ -474,7 +592,7 @@ class Bug {
       // scale(1,this.movementSpeed);
     }
     if(this.onlyMoveVertically === true) {
-      this.velocity = calculateSide(this.movementSpeed, 0)
+      this.velocity = calculateSide(this.movementSpeed, 0) //return movementspeed * 1
       this.initalY += this.velocity;
       // translate(this.initalX, this.initalY);
       // rotate(PI/2);
@@ -499,24 +617,24 @@ class Bug {
     //   this.previousMovementSpeed = this.movementSpeed;
     // }
     if(this.sizeScaling * this.initalX < 0) {
-      console.log("collided");
+      // console.log("collided");
       this.movementSpeed *= -1;
       this.spriteXCoordinate = 910;
     }
     if(this.initalX + this.sizeScaling*400 > width) {
-      console.log("collided");
+      // console.log("collided");
       this.movementSpeed *= -1;
       this.previousMovementSpeed = this.movementSpeed;
       this.spriteXCoordinate = 0;
     }
 
     if(this.sizeScaling * this.initalY < 0) {
-      console.log("collided");
+      // console.log("collided");
       this.movementSpeed *= -1;
       this.spriteYCoordinate = 910;
     }
     if(this.initalY + this.sizeScaling*400 > height) {
-      console.log("collided");
+      // console.log("collided");
       this.movementSpeed *= -1;
       this.previousMovementSpeed = this.movementSpeed;
       this.spriteYCoordinate = 0;
