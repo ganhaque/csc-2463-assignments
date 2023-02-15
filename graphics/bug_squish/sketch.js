87da7d2ce05659ced11761424952420ed8e4bca4
ganhaque <ganhaquegnq@gmail.com>
2023-02-14 22:13:06 -0600

    :sparkles: :art: death/squished sprite

--- graphics/bug_squish/sketch.js
+++ graphics/bug_squish/sketch.js
@@ -1,245 +1,3 @@
-// let spriteSheet;
-//
-// let spriteSheetFilenames = ["bug2.png" ];
-// let spriteSheets = [];
-// let animations = [];
-//
-// const GameState = {
-//   Start: "Start",
-//   Playing: "Playing",
-//   GameOver: "GameOver"
-// };
-//
-// let game = { score: 0, maxScore: 0, maxTime: 10, elapsedTime: 0, totalSprites: 15, state: GameState.Start, targetSprite: 2 };
-//
-// function preload() {
-//   for(let i=0; i < spriteSheetFilenames.length; i++) {
-//     spriteSheets[i] = loadImage("assets/" + spriteSheetFilenames[i]);
-//   }
-// }
-//
-// function setup() {
-//   createCanvas(400, 400);
-//   imageMode(CENTER);
-//   angleMode(DEGREES);
-//
-//   reset();
-// }
-//
-// function reset() {
-//   game.elapsedTime = 0;
-//   game.score = 0;
-//   game.totalSprites = random(10,30);
-//
-//   animations = [];
-//   for(let i=0; i < game.totalSprites; i++) {
-//     animations[i] = new WalkingAnimation(random(spriteSheets),32,32,random(100,300),random(100,300),9,random(0.5,1),4,random([0,1]));
-//   }
-// }
-//
-// function draw() {
-//   switch(game.state) {
-//     case GameState.Playing:
-//       background(220);
-//   
-//       for(let i=0; i < animations.length; i++) {
-//         animations[i].draw();
-//       }
-//       fill(0);
-//       textSize(40);
-//       text(game.score,20,40);
-//       let currentTime = game.maxTime - game.elapsedTime;
-//       text(ceil(currentTime), 300,40);
-//       game.elapsedTime += deltaTime / 1000;
-//
-//       if (currentTime < 0)
-//         game.state = GameState.GameOver;
-//       break;
-//     case GameState.GameOver:
-//       game.maxScore = max(game.score,game.maxScore);
-//
-//       background(0);
-//       fill(255);
-//       textSize(40);
-//       textAlign(CENTER);
-//       text("Game Over!",200,200);
-//       textSize(35);
-//       text("Score: " + game.score,200,270);
-//       text("Max Score: " + game.maxScore,200,320);
-//       break;
-//     case GameState.Start:
-//       background(0);
-//       fill(255);
-//       textSize(50);
-//       textAlign(CENTER);
-//       text("Cyclops Game",200,200);
-//       textSize(30);
-//       text("Press Any Key to Start",200,300);
-//       break;
-//   }
-// }
-//
-// function keyPressed() {
-//   switch(game.state) {
-//     case GameState.Start:
-//       game.state = GameState.Playing;
-//       break;
-//     case GameState.GameOver:
-//       reset();
-//       game.state = GameState.Playing;
-//       break;
-//   }
-// }
-//
-// function mousePressed() {
-//   switch(game.state) {
-//     case GameState.Playing:
-//       for (let i=0; i < animations.length; i++) {
-//         let contains = animations[i].contains(mouseX,mouseY);
-//         if (contains) {
-//           if (animations[i].moving != 0) {
-//             animations[i].stop();
-//             if (animations[i].spritesheet === spriteSheets[game.targetSprite])
-//               game.score += 1;
-//             else
-//               game.score -= 1;
-//           }
-//           else {
-//             if (animations[i].xDirection === 1)
-//               animations[i].moveRight();
-//             else
-//               animations[i].moveLeft();
-//           }
-//         }
-//       }
-//       break;
-//     // case GameState.GameOver:
-//     //   reset();
-//     //   game.state = GameState.Playing;
-//     //   break;
-//   }
-//   
-// }
-//
-// class WalkingAnimation {
-//   constructor(spritesheet, spriteWidth, spriteHeight,
-//     locationX, locationY,
-//     animationLength,
-//     movementSpeed,
-//     framerate,
-//     onlyMoveVertically = false
-//   ) {
-//     this.spritesheet = spritesheet;
-//     this.spriteWidth = spriteWidth;
-//     this.spriteHeight = spriteHeight;
-//
-//     this.locationX = locationX;
-//     this.locationY = locationY;
-//
-//     this.u = 0;
-//     this.v = 0;
-//     this.animationLength = animationLength;
-//     this.currentFrame = 0;
-//
-//     this.moving = 1;
-//     this.xDirection = 1;
-//     this.movementSpeed = movementSpeed;
-//     this.framerate = framerate*movementSpeed;
-//     this.onlyMoveVertically = onlyMoveVertically;
-//   }
-//
-//   draw() {
-//
-//     // if (this.moving != 0)
-//     //   this.u = this.currentFrame % this.animationLength;
-//     // else
-//     //   this.u = 0;
-//
-//     this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : this.u;
-//     push();
-//     translate(this.locationX,this.locationY);
-//     if (!this.onlyMoveVertically)
-//       rotate(90);
-//     scale(this.xDirection,1);
-//     
-//
-//     //rect(-26,-35,50,70);
-//
-//     image(this.spritesheet,0,0,
-//       this.spriteWidth,this.spriteHeight,
-//       this.u*this.spriteWidth,
-//       this.v*this.spriteHeight,
-//       this.spriteWidth,this.spriteHeight);
-//     pop();
-//     let proportionalFramerate = round(frameRate() / this.framerate);
-//     if (frameCount % proportionalFramerate == 0) {
-//       this.currentFrame++;
-//     }
-//   
-//     if (this.onlyMoveVertically) {
-//       this.locationY += this.moving*this.movementSpeed;
-//       this.move(this.locationY,this.spriteWidth / 4,height - this.spriteWidth / 4);
-//     }
-//     else {
-//       this.locationX += this.moving*this.movementSpeed;
-//       this.move(this.locationX,this.spriteWidth / 4,width - this.spriteWidth / 4);
-//     }
-//
-//     
-//   }
-//
-//   move(position,lowerBounds,upperBounds) {
-//     if (position > upperBounds) {
-//       this.moveLeft();
-//     } else if (position < lowerBounds) {
-//       this.moveRight();
-//     }
-//   }
-//
-//   moveRight() {
-//     this.moving = 1;
-//     this.xDirection = 1;
-//     this.v = 0;
-//   }
-//
-//   moveLeft() {
-//     this.moving = -1;
-//     this.xDirection = -1;
-//     this.v = 0;
-//   }
-//
-//   keyPressed(right, left) {
-//     if (keyCode === right) {
-//       
-//       this.currentFrame = 1;
-//     } else if (keyCode === left) {
-//
-//       this.currentFrame = 1;
-//     }
-//   }
-//
-//   keyReleased(right,left) {
-//     if (keyCode === right || keyCode === left) {
-//       this.moving = 0;
-//     }
-//   }
-//
-//   contains(x,y) {
-//     //rect(-26,-35,50,70);
-//     let insideX = x >= this.locationX - 26 && x <= this.locationX + 25;
-//     let insideY = y >= this.locationY - 35 && y <= this.locationY + 35;
-//     return insideX && insideY;
-//   }
-//
-//   stop() {
-//     this.moving = 0;
-//     this.u = 7;
-//     this.v = 8;
-//   }
-// }
-//
-// ///////////////////////////////////////////////////////////////
-
 //Canvas properties:
 let CANVAS_WIDTH = 2000;
 let CANVAS_HEIGHT = 1200;
@@ -358,7 +116,7 @@ function draw() {
   // noSmooth();
   // image(spriteSheet, 0, 200, 220, 400, 600, 0, 600, 960);
   // image(spriteSheet, 0, 200, 220, 400, 0, 1016, 600, 960);
-  // image(spriteSheet, 0, 200, 400, 220, 0, 1945, 960, 600);
+  image(spriteSheet, 0, 200, 400, 220, 0, 3725, 960, 600);
   // image(spriteSheet, 0, 200, 165, 250, 2500, 0, 500, 825);
 }
 
@@ -368,6 +126,7 @@ function restart() {
   scoreData.score = 0;
   console.log("score", gameData.score);
   gameData.amountOfBug = random(12, 30);
+  // gameData.amountOfBug = 1;
 
   bugArray = [];
   for(let i = 0; i < gameData.amountOfBug; i++) {
@@ -519,12 +278,11 @@ class Bug {
     push();
     noSmooth();
 
-    this.checkCanvasCollision();
 
-
-    let baseFramerate = 1*this.movementSpeed;
-    let proportionalFramerate = round(frameRate() / baseFramerate);
     if(this.isAlive) {
+      this.checkCanvasCollision();
+      let baseFramerate = 1*this.movementSpeed;
+      let proportionalFramerate = round(frameRate() / baseFramerate);
       if(frameCount % proportionalFramerate === 0) {
         // console.log("call");
         this.animationHandler();
@@ -538,26 +296,32 @@ class Bug {
     // console.log(this.hitboxCheck(mouseX, mouseY));
     // this.hitboxCheck(mouseX, mouseY);
     // image(spriteSheet, 0, 200, 220, 400, 0, 0, 600, 960);
-    if(this.isAlive) {
-      if(this.onlyMoveVertically) {
-        image(
-          spriteSheet, this.initalX, this.initalY,
-          this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
-          this.spriteXCoordinate + (this.spriteWidth * this.currentFrame),
-          this.spriteYCoordinate,
-          this.spriteWidth, this.spriteHeight
-        )
-      }
-      else if(this.onlyMoveHorizontally) {
-        image(
-          spriteSheet, this.initalX, this.initalY,
-          this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
-          this.spriteXCoordinate,
-          this.spriteYCoordinate + (this.spriteHeight * this.currentFrame),
-          this.spriteWidth, this.spriteHeight
-        )
-      }
+    // if(this.isAlive) {
+    if(this.onlyMoveVertically) {
+      console.log("vertical render", this.currentFrame);
+      // console.log( this.currentFrame);
+      image(
+        spriteSheet, this.initalX, this.initalY,
+        this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
+        this.spriteXCoordinate + (this.spriteWidth * this.currentFrame),
+        this.spriteYCoordinate,
+        this.spriteWidth, this.spriteHeight
+      )
     }
+    else if(this.onlyMoveHorizontally) {
+      // image(spriteSheet, 0, 200, 400, 220, 0, 3725, 960, 600);
+      console.log("hor render");
+
+      console.log(this.spriteYCoordinate + (this.spriteHeight * this.currentFrame));
+      image(
+        spriteSheet, this.initalX, this.initalY,
+        this.destinationX*this.sizeScaling, this.destinationY*this.sizeScaling,
+        this.spriteXCoordinate,
+        this.spriteYCoordinate + (this.spriteHeight * this.currentFrame),
+        this.spriteWidth, this.spriteHeight
+      )
+    }
+    // }
     pop();
   }
 
@@ -566,20 +330,21 @@ class Bug {
     // console.log("check hitbox...");
     // rect(12, 10, this.spriteWidth - 23, this.spriteHeight - 16);
     let insideX = (x <= this.initalX + this.destinationX*this.sizeScaling && x >= this.initalX);
-    console.log("condition 1:", this.initalX + this.destinationX*this.sizeScaling);
-    console.log("condition 2:", this.initalX);
-    console.log("x:", insideX);
+    // console.log("condition 1:", this.initalX + this.destinationX*this.sizeScaling);
+    // console.log("condition 2:", this.initalX);
+    // console.log("x:", insideX);
     let insideY = (y <= this.initalY + this.destinationY*this.sizeScaling && y >= this.initalY);
-    console.log("y:", insideY);
+    // console.log("y:", insideY);
     return (insideX && insideY)
   }
   squished() {
+    console.log("squished");
     this.movementSpeed = 0;
     this.isAlive = false;
-    this.spriteXCoordinate = 2500;
-    this.spriteYCoordinate = 0;
-    this.spriteWidth = 500;
-    this.currentFrame = 0;
+    // this.spriteXCoordinate = 2500;
+    // this.spriteYCoordinate = 0;
+    // this.spriteWidth = 500;
+    this.currentFrame = 3;
     // image(spriteSheet, 0, 200, 220, 400, 1800, 0, 600, 960);
     // image(spriteSheet, 0, 200, 165, 250, 2500, 0, 500, 825);
 
@@ -658,6 +423,7 @@ class Bug {
   }
 
   animationHandler() {
+    console.log("animation called");
     if(this.currentFrame < 2) {
       this.currentFrame += 1;
     }
